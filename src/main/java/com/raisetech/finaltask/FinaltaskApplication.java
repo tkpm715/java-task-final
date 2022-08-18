beef9b332eed815602efe71a7c622d5c10eba8d6
tk <pm@letter.mail-box.ne.jp>
2022-08-18 22:57:44 +0900

    初回のコミット

---
+++ src/main/java/com/raisetech/finaltask/FinaltaskApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.finaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/finaltask/FinaltaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.finaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaltaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
